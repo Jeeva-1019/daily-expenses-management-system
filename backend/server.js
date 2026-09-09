69f405675713885c2789202a46fadb379eb35e74
Jeeva-1019 <mpjeevanant1996@gmail.com>
2026-09-09 13:12:22 +0530

    Make backend port cloud ready

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const crypto = require('crypto');
 const { execFile } = require('child_process');
 const authenticateToken = require('./middleware/authMiddleware');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
